179d9cf70c3b535666b9de697c440be9c6838a3f
assane savadogo <assane.savadogo81@gmail.com>
2019-08-05 02:07:00 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,56 @@
+require 'pry'
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << genre
+    @@artists << artist
+    @@count += 1
+  end
+  
+  def self.count 
+    @@count
+  end
+  
+  def self.genres 
+    @@genres.uniq!
+  end
+  
+  def self.artists
+    @@artists.uniq!
+  end
+  
+  def self.genre_count
+    new_hash = {}
+    @@genres.each do |key|
+      if new_hash[key]
+        new_hash[key] += 1 
+      else 
+        new_hash[key] = 1
+      #binding.pry 
+      end
+    end
+    new_hash
+  end
+  
+  def self.artist_count 
+    new_hash = {}
+    @@artists.each do |key|
+      if new_hash[key]
+        new_hash[key] += 1 
+      else 
+        new_hash[key] = 1 
+      end
+    end
+    new_hash
+  end
+
+end
