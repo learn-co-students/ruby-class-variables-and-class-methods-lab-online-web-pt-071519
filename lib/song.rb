0347cded3a48e4d139f912d351ff6261dc559f0a
Colleen Halpenny <cp.halpenny@gmail.com>
2019-08-06 06:32:30 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+
+  @@count = 0
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@genres << genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres = []
+    @@genres.uniq
+end
+
+end
